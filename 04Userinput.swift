d6b06f649b525055be44d1ed554f310a0fcb1a11
Sahil Naik <nsahil992@gmail.com>
2023-12-19 20:41:55 +0530

    User input added

---
+++ 04Userinput.swift
@@ -0,0 +1,7 @@
+print("Please enter the first number: ")
+var num1 = Int(readLine()!)!
+print("Please enter the second number: ")
+var num2 = Int(readLine()!)!
+
+var sum = num1 + num2
+print("The sum of \(num1) and \(num2) is \(sum)")
