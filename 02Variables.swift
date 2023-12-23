53b212472c9c9a1579ee5e917e2955a32fd483ad
Sahil Naik <nsahil992@gmail.com>
2023-12-23 18:02:08 +0530

    Variables added

--- 02Variables.swift
+++ 02Variables.swift
@@ -3,8 +3,12 @@ var str:String = "Sahil"
 var a:Int = 100
 var b:Int = 1
 var c = a + b
+
 let apple = 3
 let applesummary = "I have \(apple) apples."
+
+let oranges = 5
+let fruitsummary = "I have total of \(apple) apples and \(oranges) oranges."
 
 print("My name is: ", str)
 print("The addition of a and b is: ", c)
@@ -15,4 +19,5 @@ print("The value of float is: ",f)
 var t:Bool = true
 print(t)
 print(applesummary)
+print(fruitsummary)
 
