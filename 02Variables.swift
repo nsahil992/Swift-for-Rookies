9bf441ec75b511cc2e627b21d65a3a883bae2576
Sahil Naik <nsahil992@gmail.com>
2024-02-25 19:26:39 +0530

    feat: Variables in Swift

--- 02Variables.swift
+++ 02Variables.swift
@@ -1,23 +1,8 @@
-var str:String = "Sahil"
+let name = "Sahil"
+print("Greetings for the day,",name)
 
-var a:Int = 100
-var b:Int = 1
-var c = a + b
+var age = 20
+print("Your age is",age)
 
-let apple = 3
-let applesummary = "I have \(apple) apples."
 
-let oranges = 5
-let fruitsummary = "I have total of \(apple) apples and \(oranges) oranges."
-
-print("My name is: ", str)
-print("The addition of a and b is: ", c)
-
-var f:Float = 92.29
-print("The value of float is: ",f)
-
-var t:Bool = true
-print(t)
-print(applesummary)
-print(fruitsummary)
 
