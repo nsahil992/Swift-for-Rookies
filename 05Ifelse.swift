a06521f90aa85fec0adc02a633441e028c38cfee
Sahil Naik <nsahil992@gmail.com>
2024-02-26 16:01:10 +0530

    feat: If else statements

--- 05Ifelse.swift
+++ 05Ifelse.swift
@@ -1,8 +1,19 @@
-print("Enter a number: ")
-var num = Int(readLine()!)!
-
-if (num > 0) {
-    print("The number is positive")
+let waterTemp = 100
+if waterTemp > 100 {
+    print("The water is boiling")
+} else if waterTemp == 100 {
+    print("The water started boiling")
 } else {
-    print("The number is negative")
+    print("The water is not boiling")
 }
+
+
+// if else using boolean 
+
+let raining = true
+if raining == true {
+    print("Carry a raincoat")
+} else {
+    print("No need to carry a raincoat")
+}
+
