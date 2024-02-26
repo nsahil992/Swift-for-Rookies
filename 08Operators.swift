3b981a8f77ab32f9a951564cac85650f0be38b45
Sahil Naik <nsahil992@gmail.com>
2024-02-26 13:59:28 +0530

    feat: Operators in Swift

---
+++ 08Operators.swift
@@ -0,0 +1,15 @@
+var a:Int = 5
+var b:Int = 10
+var c:Int = 5
+
+// Comparison operators
+print(a == c)
+print(b < c)
+print(b > a)
+print(a != b)
+
+// Logical operators
+print(a<b && b>c)
+print(a>b || a==c)
+print(!(a<b))
+
