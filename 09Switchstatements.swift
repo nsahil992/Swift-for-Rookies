ab3412e05422cf2c1e6a082b32a961cdb8c44b5d
Sahil Naik <nsahil992@gmail.com>
2024-02-27 14:51:23 +0530

    feat: Switch statements in Swift

---
+++ 09Switchstatements.swift
@@ -0,0 +1,10 @@
+let language = "San"
+switch language {
+case "En": print("The language is English")
+case "Hi": print("The language is Hindi")
+case "San": print("The language is Sanskrit")
+case "Jap": print("The language is Japenese")
+    
+default: print("Enter a correct language")
+    
+}
