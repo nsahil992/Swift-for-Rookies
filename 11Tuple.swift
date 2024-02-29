80092512d61af412ca66cc3181f5a94cfb754a66
Sahil Naik <nsahil992@gmail.com>
2024-02-29 20:10:25 +0530

    feat: Tuples in Swift

---
+++ 11Tuple.swift
@@ -0,0 +1,9 @@
+var menu = ("Spinach", 3, true)
+var (vegType, quantity, isAvailable) = menu
+print(quantity)
+
+// Another way
+
+var fruits = (Type:"Watermelon", quantity: 3, available: true)
+print(fruits.Type)
+print(fruits.2) 
