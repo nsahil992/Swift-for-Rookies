93df9b4d7a66d6d88905370e9bacc72a8c9584a2
Sahil Naik <nsahil992@gmail.com>
2024-03-05 21:37:09 +0530

    feat: Bank Project

---
+++ 17BankProject.swift
@@ -0,0 +1,29 @@
+class VirtualBankSystem {
+    var accountType = ""
+    var isOpened = true
+    func welcomeCustomer() {
+        print("Welcome to your virtual bank system.")
+    }
+    func onboardCustomerAccountOpening() {
+        print("What kind of account would you like to open?")
+        print("1. Debit account")
+        print("2. Credit account")
+    }
+    func makeAccount(numberPadKey: Int) {
+        print("The selected option is \(numberPadKey).")
+        switch numberPadKey {
+        case 1:
+            accountType = "debit"
+        case 2:
+            accountType = "credit"
+        default:
+            print("Invalid input: \(numberPadKey)")
+            return
+        }
+        print("You have opened a \(accountType) account.")
+    }
+    func transferMoney(
+        transferType: String,
+        transferAmount: Int,
+        bankAccount: inout BankAccount
+    )
