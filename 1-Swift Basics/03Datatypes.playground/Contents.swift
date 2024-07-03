a9eefcddd410f24e2bd2b26c9ff3f6db0166412c
Sahil Naik <nsahil992@gmail.com>
2024-07-03 10:36:42 +0530

    feat: Datatypes in Swift

---
+++ 1-Swift Basics/03Datatypes.playground/Contents.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+// String Datatype
+
+let name: String = "Sahil"
+var country: String = "India"
+let city: String = "Pune"
+
+print("My name is \(name) and I live in \(city) which is a city in \(country)")
+
+// Int Datatype
+
+var age: Int = 20
+var year: Int = 2024
+
+print("I am \(age) years old and in \(year)")
+
+// Float Datatype
+
+let pi: Float = 3.14
+print(pi)
+
+// Double Datatype
+
+let d: Double = 1.33333333333333
+print("Double = ", d)
+print("Double is 64 bits and Float is 32 bits and it can store value upto 6 digits. Double is used to store large decimal values upto 15 digits.")
+
+// Boolean Datatype
+
+let isAvailble: Bool = true
+let isPluggedin: Bool = false
+let isEnabled: Bool = false
+
+print("isAvailable = ", isAvailble)
+print("isPluggedin = ", isPluggedin)
+print("isEnabled = ", isEnabled)
+
+
