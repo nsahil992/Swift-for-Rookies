dc4a208593f5717a6ffc3eec675d898318f0a831
Sahil Naik <nsahil992@gmail.com>
2024-07-03 10:36:07 +0530

    feat: Constants in Swift

---
+++ 1-Swift Basics/02Constants.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+import Foundation
+
+// When you want to name a value that is not going to change then you use constant and keyword is let.
+
+let name: String = "Sahil"
+print("Hello, My name is \(name)")
+
+let pi: Float = 3.14
+let 🎲: Int = 6
+
+print("The value of Pi is: \(pi)")
+print("A 🎲 has \(🎲) sides")
