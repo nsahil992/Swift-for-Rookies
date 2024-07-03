58d4a7a5e26513f830d43930aecd74dc81fdd45c
Sahil Naik <nsahil992@gmail.com>
2024-07-03 11:57:27 +0530

    feat: While loop in Swift

---
+++ 1-Swift Basics/09WhileLoops.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+var counter = 0
+while counter <= 5 {
+    print(counter)
+    counter += 1
+}
+
+
+
+// Repeat while loop
+
+var n: Int = 11
+repeat {
+    print(n)
+    n += 1
+}
+
+while (n <= 10)
