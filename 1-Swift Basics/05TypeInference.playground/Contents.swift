af6696812e2d13cb986dbf986aaab29e8d3a1014
Sahil Naik <nsahil992@gmail.com>
2024-07-03 10:59:38 +0530

    feat: Type Inference and Type Annotation in Swift

---
+++ 1-Swift Basics/05TypeInference.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+// When you create a variable and give it a value without specifying the type then swift automatically set it for you.
+
+let role = "iOS Developer"
+// Swift has automatically set the value to type String
+
+var age = 21
+// Swift has automatically set the value to type Int
+
+var isCoding = true
+// Swift has automatically set the value to type Bool
+
+// There maybe some cases where you need to explicitly define the type which is know as Type Annotations
+
+let Role: String = "iOS Engineer"
+let Age: Int = 21
+let iSCoding: Bool = true
+
+// However it is perfectly fine to use either of the two.
