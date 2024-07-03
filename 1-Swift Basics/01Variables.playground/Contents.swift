aecd02cdff4ffadd96053acff3e977421fb4c993
Sahil Naik <nsahil992@gmail.com>
2024-07-03 10:33:18 +0530

    feat: Variables in Swift

---
+++ 1-Swift Basics/01Variables.playground/Contents.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+// Declaring variable
+var name: String
+
+// Declaring and Initialization
+var myNumber: Int = 25
+
+// Assigning a new value to a variable
+myNumber = 30
+
+// Declaring variables of different types
+var number: Int = 10
+var pi: Float = 3.14
+
+// Declaring variables without specifying the type - Type Inference
+var secondNumber = 39
+var fruit = "Apple"
+
+
+// Arithmetic operators
+var add = 5 + 10
+var subtract = 2 - 3
+var divide = 4 / 2
+var multiple = 4 * 4
+var remainder = 5 % 2
