37509a48f87b7bdf1eaf693dac09cf014221d5cf
Sahil Naik <nsahil992@gmail.com>
2024-07-05 22:32:37 +0530

    feat: Event Modifiers in Swift UI

---
+++ 2-SwiftUI/05EventModifier/05EventModifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  05EventModifier
+//
+//  Created by Sahil on 05/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2-SwiftUI/05EventModifier/05EventModifier/_5EventModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5EventModifierApp.swift
+//  05EventModifier
+//
+//  Created by Sahil on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct _5EventModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
