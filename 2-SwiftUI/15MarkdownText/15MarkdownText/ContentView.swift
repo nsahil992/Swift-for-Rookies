bb00e23b0ffbe307b920f1e1b44a7971e97bd22c
Sahil Naik <nsahil992@gmail.com>
2024-07-08 10:43:53 +0530

    feat: Markdown in Swift UI

---
+++ 2-SwiftUI/15MarkdownText/15MarkdownText/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  15MarkdownText
+//
+//  Created by Sahil on 08/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("**This is how you bold a text**.")
+        Text("*This is how you make a text italic*")
+        Text("You can [click this link](https://github.com/nsahil992/Swift-for-Rookies) to get redirected to the best SWIFT and SWIFTUI repository")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2-SwiftUI/15MarkdownText/15MarkdownText/_5MarkdownTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5MarkdownTextApp.swift
+//  15MarkdownText
+//
+//  Created by Sahil on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct _5MarkdownTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
