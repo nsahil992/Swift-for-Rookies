9e4fe9846cc890c139da750376b9c56ecc765303
Sahil Naik <nsahil992@gmail.com>
2024-07-08 10:29:32 +0530

    feat: Rotation effect in Swift UI

---
+++ 2-SwiftUI/13TextRotationEffect/13TextRotationEffect/_3TextRotationEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3TextRotationEffectApp.swift
+//  13TextRotationEffect
+//
+//  Created by Sahil on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct _3TextRotationEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
