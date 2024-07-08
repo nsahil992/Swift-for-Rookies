161a83059648447b35d8acf5abf58f553248f5d3
Sahil Naik <nsahil992@gmail.com>
2024-07-08 19:04:11 +0530

    feat: VStack, HStack and ZStack in Swift UI

--- 2-SwiftUI/06Stacks/06Stacks/ContentView.swift
+++ 2-SwiftUI/06Stacks/06Stacks/ContentView.swift
@@ -9,79 +9,127 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
-                Text("VStack")
-                    .font(.title)
-                    .foregroundStyle(.red)
-                Text("Pune")
-                    .font(.title2)
-                Text("Mumbai")
-                    .font(.title2)
-                
-                Text("HStack")
-                    .font(.title)
-                    .foregroundStyle(.blue)
-            }
-            
-            
-            
-            HStack(alignment: .center, spacing: 20) {
-                Image(systemName: "swift")
-                    .padding(10)
-                    .font(.system(size: 40))
-                
-                Text("Swift")
-                    .font(.system(size: 30, weight: .semibold, design: .rounded))
-            }
-            ZStack(alignment: .center) {
-                Rectangle()
-                    .fill(.black)
-                    .frame(width: 250, height: 250)
-                    .zIndex(0)
-                
-                Rectangle()
-                    .fill(.gray)
-                    .frame(width: 150, height: 150)
-                    .zIndex(1)
-                
-                Rectangle()
-                    .fill(.white)
-                    .frame(width: 95, height: 95)
-                    .zIndex(2)
-                
-                Text("Hi")
-                    .font(.system(size: 50))
-                    .foregroundColor(.blue)
-                    .zIndex(3)
-            }
-            
-            ZStack {
-                Image(systemName: "cloud")
-                    .padding(40)
-                    .font(.system(size: 100))
-                    .zIndex(0)
-                Text("New York")
-                    .font(.title2)
-                    .foregroundStyle(.white)
-                    .background(Color.red)
-                    .zIndex(1)
-            }
-            
-            HStack {
-                Image(systemName: "cloud")
-                    .font(.system(size: 60))
-                
-                VStack(alignment: .leading) {
-                    Text("City")
-                        .foregroundColor(.secondary)
-                    Text("New York")
-                        .font(.title)
-                }
-            }
-        }
+        HeaderView()
+        BasicAndPro()
+        Team()
     }
 }
+
+
 #Preview {
     ContentView()
+}
+
+
+
+
+struct HeaderView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Choose")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+                Text("Your Plan")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+            }
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct BasicAndPro: View {
+    var body: some View {
+        HStack(spacing: 15) {
+            VStack {
+                    Text("Basic")
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.black)
+                        .foregroundColor(.white)
+                    Text("$9")
+                        .font(.system(size: 40, weight: .black, design: .rounded))
+                        .foregroundColor(.white)
+                    Text("per month")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                
+                }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+                .padding(40)
+                .background(Color.purple)
+            .cornerRadius(10)
+        
+            VStack {
+                Text("Pro")
+                    .font(.system(.title, design: .rounded))
+                    .fontWeight(.black)
+                    .foregroundColor(.black)
+                Text("$19")
+                    .font(.system(size: 40, weight: .black, design: .rounded))
+                    .foregroundColor(.black)
+                Text("per month")
+                    .font(.headline)
+                    .foregroundColor(.secondary)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
+            .padding(40)
+            .background(Color(red: 240/255, green: 240/255, blue: 240/255))
+            .cornerRadius(10)
+            
+            .overlay(
+                Text("Best for designers")
+                .frame(width: 150, height: 25)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                .offset(x: 0, y: 90)
+            )
+        }
+        .padding(.horizontal)
+        .padding(.bottom)
+    }
+}
+
+
+struct Team: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Image(systemName: "wand.and.rays")
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+                    .padding(1)
+                Text("Team")
+                    .font(.system(.title, design: .rounded))
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                Text("$299")
+                    .font(.system(size: 40, weight: .black, design: .rounded))
+                    .foregroundColor(.white)
+                Text("per month")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            .frame(width: 280, height: 140)
+            .padding(40)
+            .background(Color(red: 62/255, green: 63/255, blue: 70/255))
+            .cornerRadius(10)
+            
+            Text("Perfect for 20 members")
+                .font(.system(.caption, design: .rounded))
+                .fontWeight(.black)
+            
+                .frame(width: 200, height: 20)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                .offset(x: 0, y: 90)
+
+            
+        }
+        Spacer()
+    }
 }
