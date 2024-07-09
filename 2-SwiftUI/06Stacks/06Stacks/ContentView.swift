58eb22d07041f2a18bb0205235fd3fc3dafd356d
Sahil Naik <nsahil992@gmail.com>
2024-07-09 17:22:37 +0530

    feat: VStack, HStack and ZStack in Swift UI

--- 2-SwiftUI/06Stacks/06Stacks/ContentView.swift
+++ 2-SwiftUI/06Stacks/06Stacks/ContentView.swift
@@ -9,24 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HeaderView()
-        BasicAndPro()
-        Team()
+        VStack {
+            HeaderView()
+            
+            HStack(spacing: 15) {
+                PricingView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple)
+                
+                ZStack {
+                    ProView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
+                    
+                    Text("Best for designers")
+                        .font(.system(.caption, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding(5)
+                        .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                        .offset(x: 0, y: 87)
+                }
+            }
+            .padding(.horizontal)
+            
+            ZStack {
+                TeamView(title: "Team", price: "$299", textColor: .white, bgColor: Color(red: 62/255, green: 63/255, blue: 70/255), icon: "wand.and.rays")
+                    .padding()
+                
+                Text("Perfect for teams with 20 members")
+                    .font(.system(.caption, design: .rounded))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .padding(5)
+                    .background(Color(red: 255/255, green: 183/255, blue: 37/255)) .offset(x: 0, y: 87)
+            }
+            Spacer()
+        }
     }
 }
-
 
 #Preview {
     ContentView()
 }
 
-
-
-
 struct HeaderView: View {
     var body: some View {
         HStack {
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 2) {
                 Text("Choose")
                     .font(.system(.largeTitle, design: .rounded))
                     .fontWeight(.black)
@@ -40,96 +66,83 @@ struct HeaderView: View {
     }
 }
 
-struct BasicAndPro: View {
+struct PricingView: View {
+    var title: String
+    var price: String
+    var textColor: Color
+    var bgColor: Color
+    
     var body: some View {
-        HStack(spacing: 15) {
-            VStack {
-                    Text("Basic")
-                        .font(.system(.title, design: .rounded))
-                        .fontWeight(.black)
-                        .foregroundColor(.white)
-                    Text("$9")
-                        .font(.system(size: 40, weight: .black, design: .rounded))
-                        .foregroundColor(.white)
-                    Text("per month")
-                        .font(.headline)
-                        .foregroundColor(.white)
-                
-                }
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-                .padding(40)
-                .background(Color.purple)
-            .cornerRadius(10)
-        
-            VStack {
-                Text("Pro")
-                    .font(.system(.title, design: .rounded))
-                    .fontWeight(.black)
-                    .foregroundColor(.black)
-                Text("$19")
-                    .font(.system(size: 40, weight: .black, design: .rounded))
-                    .foregroundColor(.black)
-                Text("per month")
-                    .font(.headline)
-                    .foregroundColor(.secondary)
-            }
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
-            .padding(40)
-            .background(Color(red: 240/255, green: 240/255, blue: 240/255))
-            .cornerRadius(10)
-            
-            .overlay(
-                Text("Best for designers")
-                .frame(width: 150, height: 25)
-                .fontWeight(.bold)
-                .foregroundColor(Color.white)
-                .background(Color(red: 255/255, green: 183/255, blue: 37/255))
-                .offset(x: 0, y: 90)
-            )
+        VStack {
+            Text(title)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .foregroundColor(textColor)
+            Text(price)
+                .font(.system(size: 40, weight: .heavy, design: .rounded))
+                .foregroundColor(textColor)
+            Text("per month")
+                .font(.headline)
+                .foregroundColor(textColor)
         }
-        .padding(.horizontal)
-        .padding(.bottom)
+        .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 100)
+        .padding(40)
+        .background(bgColor)
+        .cornerRadius(10)
     }
 }
 
-
-struct Team: View {
+struct ProView: View {
+    var title: String
+    var price: String
+    var textColor: Color
+    var bgColor: Color
     var body: some View {
-        ZStack {
-            VStack {
-                Image(systemName: "wand.and.rays")
-                    .font(.system(size: 40))
-                    .foregroundColor(.white)
-                    .padding(1)
-                Text("Team")
-                    .font(.system(.title, design: .rounded))
-                    .fontWeight(.black)
-                    .foregroundColor(.white)
-                Text("$299")
-                    .font(.system(size: 40, weight: .black, design: .rounded))
-                    .foregroundColor(.white)
-                Text("per month")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                Spacer()
-            }
-            .frame(width: 280, height: 140)
-            .padding(40)
-            .background(Color(red: 62/255, green: 63/255, blue: 70/255))
-            .cornerRadius(10)
-            
-            Text("Perfect for 20 members")
-                .font(.system(.caption, design: .rounded))
+        
+        VStack {
+            Text(title)
+                .font(.system(.title, design: .rounded))
                 .fontWeight(.black)
-            
-                .frame(width: 200, height: 20)
-                .fontWeight(.bold)
-                .foregroundColor(Color.white)
-                .background(Color(red: 255/255, green: 183/255, blue: 37/255))
-                .offset(x: 0, y: 90)
-
-            
+            Text(price)
+                .font(.system(size: 40, weight: .heavy, design: .rounded))
+            Text("per month")
+                .font(.headline)
+                .foregroundColor(textColor)
         }
-        Spacer()
+        .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 100)
+        .padding(40)
+        .background(bgColor)
+        .cornerRadius(10)
+    }
+}
+
+struct TeamView: View {
+    var title: String
+    var price: String
+    var textColor: Color
+    var bgColor: Color
+    
+    var icon: String?
+    
+    var body: some View {
+        VStack {
+               icon.map({
+                   Image(systemName: $0)
+                       .font(.largeTitle)
+                       .foregroundColor(textColor)
+       })
+               Text(title)
+                   .font(.system(.title, design: .rounded))
+                   .fontWeight(.black)
+                   .foregroundColor(textColor)
+       Text(price)
+       .font(.system(size: 40, weight: .heavy, design: .rounded)) .foregroundColor(textColor)
+               Text("per month")
+                   .font(.headline)
+                   .foregroundColor(textColor)
+       }
+       .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100) .padding(40)
+       .background(bgColor)
+       .cornerRadius(10)
     }
 }
