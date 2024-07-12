97f0a8b2d5f13ca443671a5f33faa26680f9f9ab
Sahil Naik <nsahil992@gmail.com>
2024-07-12 21:23:19 +0530

    feat: State View in SwiftUI

--- 2-SwiftUI/21SwiftUIState/21SwiftUIState/ContentView.swift
+++ 2-SwiftUI/21SwiftUIState/21SwiftUIState/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var isPlaying = false
+    @State private var counter = 1
     
     var body: some View {
         Button(action: {
@@ -18,6 +19,21 @@ struct ContentView: View {
             Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
                 .font(.system(size: 150))
                 .foregroundColor(isPlaying ? .red : .green)
+            
+            
+            Button(action: {
+                self.counter+=1
+            }) {
+                Circle()
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                    Text("\(counter)")
+                        .font(.system(size: 100, weight: .bold, design: .default))
+                        .foregroundStyle(.white)
+                    )
+                
+            }
         }
     }
 }
