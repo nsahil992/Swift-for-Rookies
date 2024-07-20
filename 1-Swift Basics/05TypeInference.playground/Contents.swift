0ca24ec7f886c1c218ae74073d0fce5fb15f2115
Sahil Naik <nsahil992@gmail.com>
2024-07-20 10:45:21 +0530

    feat: Type Inference and Type Annotation in Swift

--- 1-Swift Basics/05TypeInference.playground/Contents.swift
+++ 1-Swift Basics/05TypeInference.playground/Contents.swift
@@ -9,6 +9,7 @@ var age = 21
 // Swift has automatically set the value to type Int
 
 var isCoding = true
+var cities:[String] = []
 // Swift has automatically set the value to type Bool
 
 // There maybe some cases where you need to explicitly define the type which is know as Type Annotations
@@ -16,5 +17,14 @@ var isCoding = true
 let Role: String = "iOS Engineer"
 let Age: Int = 21
 let iSCoding: Bool = true
+let pi: Double = 3.141
+let height: Float = 1.8
+var cars: [String] = ["Lexus", "Maserati", "Range Rover", "Ferrari", "BMW", "Defender"]
+var user: [String: String] = ["user1": "user1@gmail.com"]
+var books: Set<String> = Set([
+    "Oliver Twist",
+    "48 laws of power",
+    "Tanmay Teaches Go"
+])
 
 // However it is perfectly fine to use either of the two.
