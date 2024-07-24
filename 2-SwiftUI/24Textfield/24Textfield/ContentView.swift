b1efbc0dfd68e0a784cca1c34c0f4bc7979d4762
Sahil Naik <nsahil992@gmail.com>
2024-07-24 20:42:55 +0530

    feat: TextField in SwiftUI

---
+++ 2-SwiftUI/24Textfield/24Textfield/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  24Textfield
+//
+//  Created by Sahil on 24/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2-SwiftUI/24Textfield/24Textfield/_4TextfieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4TextfieldApp.swift
+//  24Textfield
+//
+//  Created by Sahil on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct _4TextfieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
