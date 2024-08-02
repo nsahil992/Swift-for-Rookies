2b9390041632128b892780d6a539941f7a1fc480
Sahil Naik <nsahil992@gmail.com>
2024-08-02 22:08:42 +0530

    feat: Secure Field View in SwiftUI

---
+++ 2-SwiftUI/25SecureFieldView/25SecureFieldView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  25SecureFieldView
+//
+//  Created by Sahil on 02/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2-SwiftUI/25SecureFieldView/25SecureFieldView/_5SecureFieldViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5SecureFieldViewApp.swift
+//  25SecureFieldView
+//
+//  Created by Sahil on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct _5SecureFieldViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
