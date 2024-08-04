b6ef6fb6425bb9e4698708073aff38a3d9a697e8
Sahil Naik <nsahil992@gmail.com>
2024-08-04 16:14:07 +0530

    feat: State in SwiftUI

--- 2-SwiftUI/21SwiftUIState/21SwiftUIState/ContentView.swift
+++ 2-SwiftUI/21SwiftUIState/21SwiftUIState/ContentView.swift
@@ -11,30 +11,59 @@ struct ContentView: View {
     
     @State private var isPlaying = false
     @State private var counter = 1
+    @State private var title = "Default title"
+    @State private var val = "Invalid"
     
     var body: some View {
-        Button(action: {
-            self.isPlaying.toggle()
-        }) {
-            Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
-                .font(.system(size: 150))
-                .foregroundColor(isPlaying ? .red : .green)
-            
-            
-            Button(action: {
-                self.counter+=1
-            }) {
-                Circle()
-                    .frame(width: 200, height: 200)
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    .overlay(
-                    Text("\(counter)")
-                        .font(.system(size: 100, weight: .bold, design: .default))
-                        .foregroundStyle(.white)
-                    )
-                
+
+        VStack {
+            Text(title)
+            Button {
+                title = "My new title"
+            } label: {
+                Text("Change")
             }
         }
+        .padding()
+        .border(Color.black)
+        
+        VStack {
+            Text(val)
+            Button {
+                val = "Valid✅"
+            } label: {
+                Text("Check")
+            }
+        }
+        .padding()
+        .border(Color.black)
+        .padding()
+        
+        HStack {
+            Button(action: {
+                self.isPlaying.toggle()
+            }) {
+                Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
+                    .font(.system(size: 50))
+                    .foregroundColor(isPlaying ? .red : .green)
+                
+                
+                Button(action: {
+                    self.counter+=1
+                }) {
+                    Circle()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .overlay(
+                            Text("\(counter)")
+                                .font(.system(size: 30, weight: .bold, design: .default))
+                                .foregroundStyle(.white)
+                        )
+                }
+            }
+        }
+        .padding()
+        .border(Color.black)
     }
 }
 
