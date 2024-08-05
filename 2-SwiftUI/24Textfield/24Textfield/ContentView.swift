a24b7624d87e39a3d5d9f09def67bb334a70c4a7
Sahil Naik <nsahil992@gmail.com>
2024-08-05 11:11:31 +0530

    feat: Textfields in SwiftUI

--- 2-SwiftUI/24Textfield/24Textfield/ContentView.swift
+++ 2-SwiftUI/24Textfield/24Textfield/ContentView.swift
@@ -8,10 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var title: String = "Default title"
+    @State private var titleInput: String = ""
+    @State private var greet: String = "Hello, "
+    @State private var name: String = ""
     var body: some View {
-        VStack(spacing: 15) {
-            Text(title)
+        VStack {
+            VStack(spacing: 15) {
+                Text(title)
+                    .lineLimit(1)
+                    .padding()
+                    .background(Color.yellow)
+                    .cornerRadius(10)
+                TextField("Insert title", text: $titleInput)
+                    .textFieldStyle(.roundedBorder)
+                    .submitLabel(.continue)
+                    .onSubmit {
+                        assignTitle()
+                    }
+                    .padding()
+                Button {
+                    
+                } label: {
+                    Text("Save".uppercased())
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                
+            }
+            .padding(50)
         }
+
+        
+        VStack(spacing: 15) {
+            Text(greet)
+                .lineLimit(1)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .fontDesign(.rounded)
+                .padding()
+                .background(Color.gray.opacity(0.5))
+                .cornerRadius(10)
+            TextField("Enter your name...", text: $name)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+                .submitLabel(.return)
+                .onSubmit {
+                    greetName()
+                }
+            Button {
+                
+            } label: {
+                Text("Save".uppercased())
+                    .foregroundStyle(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            
+        }
+    }
+    func assignTitle() {
+        title = titleInput
+        titleInput = ""
+    }
+    func greetName() {
+        greet = "Hello, \(name)"
+        name = ""
     }
 }
 
