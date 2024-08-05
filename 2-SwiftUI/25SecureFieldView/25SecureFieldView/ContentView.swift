f3ec75e51ef0e8e6e19748c36297fdbcdbcdb945
Sahil Naik <nsahil992@gmail.com>
2024-08-05 11:22:13 +0530

    feat: Securefield in SwiftUI

--- 2-SwiftUI/25SecureFieldView/25SecureFieldView/ContentView.swift
+++ 2-SwiftUI/25SecureFieldView/25SecureFieldView/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var password: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack(alignment: .leading, spacing: 15) {
+                Text("Enter your password: ")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .fontDesign(.rounded)
+                SecureField("Enter your password", text: $password)
+                    .textFieldStyle(.roundedBorder)
+            }
+            
+            .padding()
         }
-        .padding()
+        Button {
+            password = ""
+        } label: {
+            Text("Save".uppercased())
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+        Spacer()
     }
 }
 
