f428b33610db01beba065f8f750d3e45fda5a00a
Sahil Naik <nsahil992@gmail.com>
2024-08-15 19:23:17 +0530

    feat: Animation basics in Swift UI

--- 2-SwiftUI/23AnimateBasics/23AnimateBasics/ContentView.swift
+++ 2-SwiftUI/23AnimateBasics/23AnimateBasics/ContentView.swift
@@ -8,25 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var circleColorChanged = false
     @State private var heartColorChanged = false
-    @State private var heartSizeChanged = false
+    @State private var heartsSizeChanged = false
+    
     var body: some View {
         ZStack {
             Circle()
-                .frame(width: 200, height: 200) .foregroundColor(circleColorChanged ? Color(.systemYellow) : .red)
+                .frame(width: 200, height: 200)
+                .foregroundColor(circleColorChanged ? .teal : .red)
             Image(systemName: "heart.fill")
-                .foregroundColor(heartColorChanged ? .red : .white)
+                .foregroundColor(heartColorChanged ? .white : .white)
                 .font(.system(size: 100))
-                .scaleEffect(heartSizeChanged ? 1.0 : 0.5)
+                .scaleEffect(heartsSizeChanged ? 1.0 : 0.5)
         }
         .onTapGesture {
-            self.circleColorChanged.toggle()
-            self.heartColorChanged.toggle()
-            self.heartSizeChanged.toggle()
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.1, blendDuration: 0.1))
+            {
+                self.circleColorChanged.toggle()
+                self.heartColorChanged.toggle()
+            }
+            self.heartsSizeChanged.toggle()
         }
     }
 }
 #Preview {
     ContentView()
 }
+
+/*
+ The spring animation simulates the effect of a spring moving towards a rest position. It’s useful for creating bouncy, natural-feeling animations.
+ 
+ 
+ 
+ The response parameter controls how quickly the spring reaches its final state. It’s like setting the stiffness of the spring.
+ A lower value makes the spring move faster (more stiff), while a higher value makes it slower (less stiff).
+ 
+ 
+ 
+ The dampingFraction parameter controls how much the spring oscillates as it settles. It affects the "bounciness" of the animation.
+ A value of 1.0 means no bounce; the spring will smoothly come to rest.
+ 
+ 
+ 
+ The blendDuration parameter is used to smoothly blend between animations when multiple animations are chained or overlapped.
+ */
