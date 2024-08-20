7cd8445666a0761101fd5af4e57536cbccb0e535
Sahil Naik <nsahil992@gmail.com>
2024-08-20 21:16:25 +0530

    feat: Closures in Swift

--- 1-Swift Basics/17Closures.playground/Contents.swift
+++ 1-Swift Basics/17Closures.playground/Contents.swift
@@ -2,6 +2,14 @@ import Foundation
 
 // Closures are functions without name
 
+let multiplier = { (num1: Int, num2: Int) -> Int in
+    let result = num1 * num2
+    return result
+}
+
+print("The total is: \(multiplier(1, 2))")
+
+
 var india = {
     print("Welcome to Rajasthan 🐪")
 }
