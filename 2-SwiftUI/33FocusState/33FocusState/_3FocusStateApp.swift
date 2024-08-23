05111c78db3af9b033cac6fcd28105440d458263
Sahil Naik <nsahil992@gmail.com>
2024-08-23 22:19:34 +0530

    feat: Focus State in SwiftUI

---
+++ 2-SwiftUI/33FocusState/33FocusState/_3FocusStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3FocusStateApp.swift
+//  33FocusState
+//
+//  Created by Sahil on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct _3FocusStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
