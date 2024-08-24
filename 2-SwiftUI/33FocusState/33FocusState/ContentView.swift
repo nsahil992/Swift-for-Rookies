41fff1a4fbb88718542478d6b38c77d42f88691f
Sahil Naik <nsahil992@gmail.com>
2024-08-24 17:39:53 +0530

    feat: Focus State in SwiftUI

--- 2-SwiftUI/33FocusState/33FocusState/ContentView.swift
+++ 2-SwiftUI/33FocusState/33FocusState/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+enum FocusName {
+    case name, surname
+}
+
 struct ContentView: View {
+    @FocusState var focusName: FocusName?
+    @State private var title: String = "Default title"
+    @State private var nameInput: String = ""
+    @State private var surnameInput: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text(title)
+                .padding()
+                .lineLimit(1)
+                .background(Color.yellow)
+            TextField("Insert name", text: $nameInput)
+                .padding(4)
+                .textFieldStyle(.roundedBorder)
+                .background(focusName == .name ? Color(white : 0.9) : .white)
+                .focused($focusName, equals: .name)
+            TextField("Insert surname", text: $surnameInput)
+                .padding(4)
+                .textFieldStyle(.roundedBorder)
+                .background(focusName == .surname ? Color(white : 0.9) : .white)
+                .focused($focusName, equals: .surname)
+            
+            Button {
+                title = nameInput + " " + surnameInput
+                nameInput = ""
+                surnameInput = ""
+            } label: {
+                Text("Save")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
-        .padding()
     }
 }
 
