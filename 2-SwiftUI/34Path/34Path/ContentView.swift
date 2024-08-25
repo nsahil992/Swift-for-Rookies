456851eb28955dd5aa14509472f5b0d6d3f1467f
Sahil Naik <nsahil992@gmail.com>
2024-08-25 22:15:19 +0530

    feat: Path in SwiftUI

---
+++ 2-SwiftUI/34Path/34Path/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  34Path
+//
+//  Created by Sahil on 25/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Path() {path in
+            path.move(to: CGPoint(x: 20, y: 20))
+                path.addLine(to: CGPoint(x: 300, y: 20))
+                path.addLine(to: CGPoint(x: 300, y: 200))
+                path.addLine(to: CGPoint(x: 20, y: 200))
+
+        }
+        .fill(.green)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2-SwiftUI/34Path/34Path/_4PathApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4PathApp.swift
+//  34Path
+//
+//  Created by Sahil on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct _4PathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
