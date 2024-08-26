1961caded81c3e89d63a11922c5e63eb27cdb2b1
Sahil Naik <nsahil992@gmail.com>
2024-08-26 21:37:15 +0530

    feat: Path in SwiftUI

--- 2-SwiftUI/34Path/34Path/ContentView.swift
+++ 2-SwiftUI/34Path/34Path/ContentView.swift
@@ -11,12 +11,14 @@ struct ContentView: View {
     var body: some View {
         Path() {path in
             path.move(to: CGPoint(x: 20, y: 20))
-                path.addLine(to: CGPoint(x: 300, y: 20))
-                path.addLine(to: CGPoint(x: 300, y: 200))
-                path.addLine(to: CGPoint(x: 20, y: 200))
-
+            path.addLine(to: CGPoint(x: 300, y: 20))
+            path.addLine(to: CGPoint(x: 300, y: 200))
+            path.addLine(to: CGPoint(x: 20, y: 200))
+            path.closeSubpath()
+            
         }
-        .fill(.green)
+        .stroke(.green, lineWidth: 10)
+        
     }
 }
 
